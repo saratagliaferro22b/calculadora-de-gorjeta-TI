b6a56501eda058d1d5d941da0891ca2332241128
saratagliaferro <saratagliaferro@outlook.com>
2024-03-30 18:22:15 -0300

    terceiro commit do html

---
+++ script.js
@@ -0,0 +1,8 @@
+let conta = 0 
+
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input",)
+
+function receberValorconta(evento) {
+    console.log(evento.target.value)
+}
